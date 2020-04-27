c62e918ca94f43a1aafe2e2acc63e2c49b79d3ec
morzw <morzw@post.bgu.ac.il>
2020-04-27 18:20:50 +0300

    MyDecompressorInputStream

--- src/IO/MyDecompressorInputStream.java
+++ src/IO/MyDecompressorInputStream.java
@@ -1,4 +1,84 @@
 package IO;
 
-public class MyDecompressorInputStream {
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+public class MyDecompressorInputStream extends InputStream {
+
+    private InputStream in;
+
+    public MyDecompressorInputStream(InputStream in) {
+        this.in = in;
+    }
+
+    @Override
+    public int read() throws IOException {
+        return 0;
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        //read from input stream
+        int len = ((b.length-24)/8)+24;
+        byte[] comp = new byte[len];
+        in.read(comp);
+        in.close();
+
+        //copy the first 24 cells
+        for (int i=0; i<24; i++)
+        {
+            b[i] = comp[i];
+        }
+
+        //4 cells represents byte --> int --> binary
+        byte[] temp = new byte[4];
+        int resInt;
+        String resStr;
+        for (int i=24; i<comp.length; i=i+4)
+        {
+            temp[0] = comp[i];
+            temp[1] = comp[i+1];
+            temp[2] = comp[i+2];
+            temp[3] = comp[i+3];
+            resInt = fromByteToInt(temp); //byte --> int
+            resStr = fromDecimalToBinary(resInt); //int --> binary (string)
+            for (int j=0; j<resStr.length(); j++)
+            {
+                String tempStr = ""+resStr.charAt(j); //each char --> string
+                int digit = Integer.parseInt(tempStr); //string (with one char) --> int
+                b[j+24] = (byte) digit; //int --> byte
+            }
+        }
+        return 0;
+    }
+
+    private int fromByteToInt(byte[] bm)
+    {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bm);
+        return byteBuffer.getInt();
+    }
+
+    private String fromDecimalToBinary(int num)
+    {
+        String str = "";
+        //convert to binary
+        while (num > 0)
+        {
+            int digit = num % 2;
+            num = num / 2;
+            str = digit + str;
+        }
+        //add 0's to complete to 32
+        if (str.length() < 32)
+        {
+            String zero = "";
+            for (int i=0; i<(32-str.length()); i++)
+            {
+                zero = "0" + zero;
+            }
+            str = zero + str;
+        }
+        return str;
+    }
 }
--- src/test/RunCompressDecompressMaze.java
+++ src/test/RunCompressDecompressMaze.java
@@ -8,61 +8,61 @@ import algorithms.mazeGenerators.MyMazeGenerator;
 
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 
 public class RunCompressDecompressMaze {
     public static void main(String[] args) {
-
-        //test- toByteArray
-        Maze m = new MyMazeGenerator().generate(4,500);
-        m.print();
-
-        byte[] bm = m.toByteArray();
-
-        for (int i=0; i<bm.length; i++)
-        {
-            System.out.println(bm[i]);
+        String mazeFileName = "savedMaze.maze";
+        AMazeGenerator mazeGenerator = new MyMazeGenerator();
+        Maze maze = mazeGenerator.generate(100, 100); //Generate new maze
+        try {
+            // save maze to a file
+            OutputStream out = new MyCompressorOutputStream(new FileOutputStream(mazeFileName));
+            out.write(maze.toByteArray());
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        byte savedMazeBytes[] = new byte[0];
+        try {
+            //read maze from file
+            InputStream in = new MyDecompressorInputStream(new FileInputStream(mazeFileName));
+            savedMazeBytes = new byte[maze.toByteArray().length];
+            in.read(savedMazeBytes);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Maze loadedMaze = new Maze(savedMazeBytes);
+        boolean areMazesEquals =
+                Arrays.equals(loadedMaze.toByteArray(),maze.toByteArray());
+        System.out.println(String.format("Mazes equal: %s",areMazesEquals)); //maze should be equal to loadedMaze
 
-        //test- fromByteArrayToMaze
-        System.out.println("########");
-        Maze m2 = new Maze(bm);
-        m2.print();
-        System.out.println("********");
-        System.out.println(m2.getRows());
-        System.out.println("********");
-        System.out.println(m2.getCols());
-        System.out.println("********");
-        System.out.println(m2.getStartPosition());
-        System.out.println("********");
-        System.out.println(m2.getGoalPosition());
-
-//        String mazeFileName = "savedMaze.maze";
-//        AMazeGenerator mazeGenerator = new MyMazeGenerator();
-//        Maze maze = mazeGenerator.generate(100, 100); //Generate new maze
-//        try {
-//            // save maze to a file
-//            OutputStream out = new MyCompressorOutputStream(new FileOutputStream(mazeFileName));
-//            out.write(maze.toByteArray());
-//            out.flush();
-//            out.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
+//        //test- toByteArray
+//        Maze m = new MyMazeGenerator().generate(4,500);
+//        m.print();
+//
+//        byte[] bm = m.toByteArray();
+//
+//        for (int i=0; i<bm.length; i++)
+//        {
+//            System.out.println(bm[i]);
 //        }
-//        byte savedMazeBytes[] = new byte[0];
-//        try {
-//            //read maze from file
-//            InputStream in = new MyDecompressorInputStream(new FileInputStream(mazeFileName));
-//            savedMazeBytes = new byte[maze.toByteArray().length];
-//            in.read(savedMazeBytes);
-//            in.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        Maze loadedMaze = new Maze(savedMazeBytes);
-//        boolean areMazesEquals =
-//                Arrays.equals(loadedMaze.toByteArray(),maze.toByteArray());
-//        System.out.println(String.format("Mazes equal: %s",areMazesEquals)); //maze should be equal to loadedMaze
+//
+//        //test- fromByteArrayToMaze
+//        System.out.println("########");
+//        Maze m2 = new Maze(bm);
+//        m2.print();
+//        System.out.println("********");
+//        System.out.println(m2.getRows());
+//        System.out.println("********");
+//        System.out.println(m2.getCols());
+//        System.out.println("********");
+//        System.out.println(m2.getStartPosition());
+//        System.out.println("********");
+//        System.out.println(m2.getGoalPosition());
     }
 
 }
